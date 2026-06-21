c1878619aca46ab4eb48bc3620ccf9b94618de10
ramielchik01 <ramiel_0101@icloud.com>
2026-06-21 00:14:45 -0700

    Auto-update

--- backend/service/user_service.py
+++ backend/service/user_service.py
@@ -2,4 +2,5 @@ from model.user import UserRegister
 class UserService:
     def __init__(self):
         pass
-    def registerNewUser(self, userData:)
+    def registerNewUser(self, userData: UserRegister)
+        
